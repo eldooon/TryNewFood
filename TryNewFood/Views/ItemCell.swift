9a54091ff0e5c87fe6a5023f5049c6ad9f735ab9
eldooon <eldonchan91@gmail.com>
2018-01-16 14:28:54 -0500

    Fixed itemcell constraint issue

--- TryNewFood/Views/ItemCell.swift
+++ TryNewFood/Views/ItemCell.swift
@@ -36,7 +36,7 @@ class ItemCell: UICollectionViewCell {
         super.init(frame: frame)
         
         addSubview(itemImageView)
-        itemImageView.anchor(centerX: self.centerXAnchor, centerY: self.centerYAnchor, top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 50, paddingRight: 0, width: 0, height: 0)
+        itemImageView.anchor(centerX: nil, centerY: nil, top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 50, paddingRight: 0, width: 0, height: 0)
         
         addSubview(itemNameLabel)
         itemNameLabel.anchor(centerX: nil, centerY: nil, top: itemImageView.bottomAnchor, left: itemImageView.leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 5, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
