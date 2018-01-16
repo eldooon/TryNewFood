c52506b724dc872d6c833c846126285bbe8e819b
eldooon <eldonchan91@gmail.com>
2018-01-16 14:23:08 -0500

    removed fucntion on itemcategory class

--- TryNewFood/Model/ItemCategory.swift
+++ TryNewFood/Model/ItemCategory.swift
@@ -14,24 +14,5 @@ class ItemCategory {
     var name = String()
     var items = [Item]()
     
-//    static func retrieveData(completion: @escaping (ItemCategory) -> ()) {
-//        
-//        let ref = Database.database().reference(withPath: "Food")
-//        ref.observe(.value, with: { (snapshot) in
-//            
-//            let itemCategory = ItemCategory()
-//            itemCategory.name = "Food"
-//            guard let dictionaries = snapshot.value as? [String: Any] else { return }
-//            
-//            dictionaries.forEach({ (key, value) in
-//                guard let dictionary = value as? [String: Any] else { return }
-//                let item = Item(dictionary: dictionary)
-//                itemCategory.items?.append(item)
-//                //                dump(item)
-//            })
-//            completion(itemCategory)
-//        })
-//    }
-//    
 }
 
