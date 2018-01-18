a7580b6839fca1fe3430f78743cc353e8d464f0a
eldooon <eldonchan91@gmail.com>
2018-01-18 14:31:34 -0500

    Working FeaturedItemCell

--- TryNewFood/Views/FeaturedCell.swift
+++ TryNewFood/Views/FeaturedCell.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class FeaturedCell: CategoriesCell {
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        itemCollectionView.dataSource = self
+        itemCollectionView.delegate = self
+        itemCollectionView.register(FeaturedItemCell.self, forCellWithReuseIdentifier: cellId)
+        itemCollectionView.reloadData()
+        createLayout()
+        createObserver()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if let count = itemCategory?.items.count {
@@ -19,7 +32,7 @@ class FeaturedCell: CategoriesCell {
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ItemCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! FeaturedItemCell
 
         cell.itemImageView.image = itemCategory?.items[indexPath.item].image
         cell.itemNameLabel.text = itemCategory?.items[indexPath.item].name
--- TryNewFood/Views/FeaturedItemCell.swift
+++ TryNewFood/Views/FeaturedItemCell.swift
@@ -33,11 +33,12 @@ class FeaturedItemCell: UICollectionViewCell {
         itemImageView.anchor(centerX: nil, centerY: nil, top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 50, paddingRight: 0, width: 0, height: 0)
         
         addSubview(itemNameLabel)
-        itemNameLabel.anchor(centerX: nil, centerY: nil, top: itemImageView.bottomAnchor, left: itemImageView.leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 5, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        itemNameLabel.anchor(centerX: nil, centerY: nil, top: itemImageView.bottomAnchor, left: itemImageView.leftAnchor, bottom: nil, right: nil, paddingTop: 10, paddingLeft: 5, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
 }
-
