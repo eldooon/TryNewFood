0f4ff33bc4ce71a20fe0853b23c2a9c8e2946996
eldooon <eldonchan91@gmail.com>
2018-01-22 14:50:59 -0500

    Added featuredDetailcell

---
+++ TryNewFood/Views/FeaturedDetailCell.swift
@@ -0,0 +1,29 @@
+//
+//  FeaturedDetailCell.swift
+//  TryNewFood
+//
+//  Created by Eldon Chan on 1/22/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedDetailCell: UICollectionViewCell {
+    
+    let textView: UITextView = {
+        let tv = UITextView()
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(textView)
+        textView.anchor(centerX: nil, centerY: nil, top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 10, paddingRight: 10, width: 0, height: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
